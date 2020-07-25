33463640a2fdee7d41525f8bce7af8d741394069
Dmitry Ivanov <dimiv007@gmail.com>
2020-07-25 15:39:52 +0300

    Add Dockerfile

--- settings.py
+++ settings.py
@@ -1,18 +1,21 @@
+import os
+
 #Configuration
 
 DATETIME_FORMAT = '%Y/%d/%m %H:%M:%S'
 DATETIME_FORMAT_CHARTS = '%Y-%d-%m-%H:%M:%S'
 
-EXPLORER_API_URL = 'http://localhost:8080/api/v1/explorer/'
+TRANSACTIONS_API_ENDPOINT = os.getenv('TRANSACTIONS_API_ENDPOINT', 'http://localhost:8080/')
+
+EXPLORER_API_URL = f'{TRANSACTIONS_API_ENDPOINT}api/v1/explorer/'
 EXPLORER_ITEM_LIMIT = 50
 
-CHARTS_API_URL = 'http://0.0.0.0:8080/api/v1/analytics/'
+CHARTS_API_URL = f'{TRANSACTIONS_API_ENDPOINT}api/v1/analytics/'
 CHART_INTERVAL_MAP = {
 	'1m' : 60,
 	'1h' : 3600,
 	'1d' : 86400
 }
-
 CHART_POINTS_AMOUNT = 100
 
-COMMON_API_URL = 'http://0.0.0.0:8080/api/v1/'
+COMMON_API_URL = f'{TRANSACTIONS_API_ENDPOINT}api/v1/'
